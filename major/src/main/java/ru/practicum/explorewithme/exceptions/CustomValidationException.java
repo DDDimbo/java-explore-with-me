16165368815fac5833d4ef5024cba4cda7099654
DDDimbo <vinokurov1540@bk.ru>
2022-12-08 04:29:19 +0300

    style: косметический ремонт

--- major/src/main/java/ru/practicum/explorewithme/exceptions/CustomValidationException.java
+++ major/src/main/java/ru/practicum/explorewithme/exceptions/CustomValidationException.java
@@ -1,10 +1,6 @@
 package ru.practicum.explorewithme.exceptions;
 
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.util.Set;
-
 public class CustomValidationException extends RuntimeException {
 
     public CustomValidationException(String message) {
